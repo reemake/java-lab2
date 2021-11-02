70a5a7fa3141664eb7a38bf929d0a92ed24c1e7c
reemake <safnikita@inbox.ru>
2021-11-02 18:34:14 +0300

    Added javadoc

--- src/main/java/calculator/RpnEvaluation.java
+++ src/main/java/calculator/RpnEvaluation.java
@@ -3,8 +3,19 @@ package calculator;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
+/**
+ * Class which is describing methods for parsing an expression and calculating its value using RPN (Reverse Polish Notation)
+ * @author Nikita Safonov, student of AMM VSU, 3rd year, 3rd group
+ */
 public class RpnEvaluation {
 
+    /**
+     * Calculating the value of an arithmetic operation
+     * @param op1 first operand
+     * @param op2 second operand
+     * @param operator arithmetic operation
+     * @return the value of the calculated arithmetic operation
+     */
     public static double someCalc(double op1, double op2, String operator){
         switch (operator){
             case "+":
@@ -21,6 +32,11 @@ public class RpnEvaluation {
         return -1;
     }
 
+    /**
+     * Method for determining the priority of arithmetic operations
+     * @param c arithmetic operation
+     * @return priority
+     */
     public static int operatorPriority(String c) {
         if (c.equals("+") || c.equals("-")) {
             return 1;
@@ -31,10 +47,20 @@ public class RpnEvaluation {
         } else return -1;
     }
 
+    /**
+     * Checking if the string is an arithmetic operation
+     * @param c element (string) of the expression
+     * @return true if element is an arithmetic operation, false otherwise
+     */
     public static boolean isOperator(String c) {
         return c.equals("+") || c.equals("-") || c.equals("*") || c.equals("/") || c.equals("^")|| c.equals("(") || c.equals(")");
     }
 
+    /**
+     * Converting an infix expression to a postfix expression
+     * @param infixExpr infix expression
+     * @return postfix expression
+     */
     public static String infixToPostfix(String infixExpr) {
         StringTokenizer infix = new StringTokenizer(infixExpr, "+-*/^()", true);
         Stack<String> stack = new Stack<>();
@@ -72,6 +98,11 @@ public class RpnEvaluation {
     }
 
 
+    /**
+     * Evaluating the value of a postfix expression
+     * @param postfixExpr postfix expression
+     * @return evaluated value
+     */
     public static double postfixEvaluation(String postfixExpr) {
         StringTokenizer postfix = new StringTokenizer(postfixExpr, " ");
         Stack<Double> opStack = new Stack<>();
